0a7052399f9be6598f7514ea2fdcca84378ea594
bulletproofsysadmin <the@bulletproofsysadmin.com>
2015-11-02 12:32:36 -0800

    most depends in for building rsyslog from latest source

--- rsyslog/supervisor/firstrun.sh
+++ rsyslog/supervisor/firstrun.sh
@@ -7,6 +7,9 @@
 sleep 8
 
 rm -f /etc/rsyslog.d/listen.conf
-sed -i -e "s/{LOGSTASHLINK}/$LOGSTASH_1_PORT_9995_TCP_ADDR/g" /etc/rsyslog.d/15_logstashjson.conf
+#sed -i -e "s/{LOGSTASHLINK}/$LOGSTASH_1_PORT_9995_TCP_ADDR/g" /etc/rsyslog.d/15_logstashjson.conf
+
+sed -i -e "s/{REDIS}/$REDIS_1_PORT_6379_TCP_ADDR/g" /etc/rsyslog.d/20_redis.conf
+
 touch '/root/.firstrun/done'
 
